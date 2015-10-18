3782eb6642a579283e1a2be5aa07ea8aa94dee25
liuqingxiangxx <863822280@qq.com>
2015-10-18 10:49:34 +0800

    我的Java程序

---
+++ src/jay.java
@@ -0,0 +1,19 @@
+
+import java.applet.*;
+import java.awt.*;
+public class jay extends Applet {
+	public void paint (Graphics g){
+	   g.drawLine(100,12,100,200);
+	   g.setColor(Color.red);
+	   g.fillOval(80, 25, 40, 40);
+	   g.setColor(Color.green);
+	   g.fillRect(80, 65, 40, 15);
+	   g.setColor(Color.red);
+	   g.fillOval(85, 79, 30,40);
+	   g.setColor(Color.green);
+	   g.fillRect(80,120, 40, 15);
+	   g.setColor(Color.red);
+	  g.fillOval(80, 135, 40, 40);
+	   }
+	}
+
